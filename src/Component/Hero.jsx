6da01313569767dea9033827efd8a6bd1d86555d
yash-matchmyflight <145266444+yash-matchmyflight@users.noreply.github.com>
2023-09-22 10:11:58 +0530

    first

---
+++ src/Component/Hero.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <div>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+              Bharat Vidya
+            </h1>
+            <p className="mb-8 leading-relaxed">
+              Our vision is to create a comprehensive educational platform named
+              “Bharat Vidya” that leverages technology to improve the quality of
+              education in rural areas.
+            </p>
+            <div className="flex justify-center">
+              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                <Link to="/login">Login</Link>
+              </button>
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+                <Link to="/register">Register</Link>
+              </button>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="https://file-uploads.teachablecdn.com/a716a5396fb0464f92ce88707de28ba1/2fa0457a06104a2897e1ef527979e8fe"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Component/Start.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+const Start = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default Start;
